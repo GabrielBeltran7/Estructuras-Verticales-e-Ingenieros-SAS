e11ce4e1d338ae4c0bb882f29140dfaeab66f1c3
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-01-21 16:25:04 -0500

    actualizar imagenes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import FaqSection from "./Components/FAQSection/FaqSection";
 import Image from "next/image";
 import ContactButtons from "./Components/ContactButtons/ContactButtons";
 
-const img ="https://res.cloudinary.com/dby8lelja/image/upload/v1737492600/Estructuras%20Verticales%20e%20Ingenieros%20SAS/servicio_de_interventoria_supervision_de_obras_y_dise%C3%B1o_estructural_j6rnjl.png";
+const img = "https://res.cloudinary.com/dby8lelja/image/upload/f_webp/v1737492600/Estructuras%20Verticales%20e%20Ingenieros%20SAS/servicio_de_interventoria_supervision_de_obras_y_dise%C3%B1o_estructural_j6rnjl.png";
 
 export default function Home() {
   return (
@@ -43,16 +43,16 @@ export default function Home() {
           </h1>
 
           <div className={styles.heroImageContainer}>
-          <Image
-    src={img}
-    alt="Servicio de Interventoría, Supervisión de Obras y Diseño Estructural en Colombia"
-    width={500}  // Asegúrate de que el tamaño es apropiado
-    height={300} // Asegúrate de que el tamaño es apropiado
-    priority  // Cargar esta imagen con alta prioridad
-    loading="eager"  // Asegura que la imagen se cargue de forma prioritaria para mejorar el LCP
-    className={styles.heroImage}
-    quality={75}  // Ajusta la calidad de la imagen si es necesario
-  />
+            <Image
+              src={img}
+              alt="Servicio de Interventoría, Supervisión de Obras y Diseño Estructural en Colombia"
+              width={500}
+              height={400}
+              priority
+              loading="eager"
+              className="heroImage"
+              quality={75}
+            />
           </div>
 
           <p>
