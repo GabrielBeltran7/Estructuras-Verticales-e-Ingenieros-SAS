6f25bf9a32b0770eed0544e5179682f2711061b9
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-01-21 19:44:40 -0500

    creando componente porque elegirnos

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+module.exports = {
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
-export default nextConfig;
 
