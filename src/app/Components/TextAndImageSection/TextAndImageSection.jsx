242a9bf84d91d6c54ecebefe6e5b8c02a407eb83
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-01-21 20:09:22 -0500

    actualizar componente quienes somos

--- src/app/Components/TextAndImageSection/TextAndImageSection.jsx
+++ src/app/Components/TextAndImageSection/TextAndImageSection.jsx
@@ -5,7 +5,7 @@ import styles from './TextAndImageSection.module.css';
 
 const TextAndImageSection = ({ imageUrl, altText, title, description }) => {
   return (
-    <Grid container spacing={8} alignItems="center" className={styles.container}>
+    <Grid container spacing={0} alignItems="center" className={styles.container}>
       <Grid item xs={12} md={6}>
         <Typography variant="h5" gutterBottom className={styles.title}>
           {title}
