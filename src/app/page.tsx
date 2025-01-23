fc8766c83ddee86872cb449f90c7409bd5cc1dad
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-01-23 11:02:43 -0500

    actualizacion encabezado de servicios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,43 +7,14 @@ import FaqSection from "./Components/FAQSection/FaqSection";
 import ContactButtons from "./Components/ContactButtons/ContactButtons";
 import TextAndImageSection from './Components/TextAndImageSection/TextAndImageSection';
 import Footer from "./Components/Footer/Footer"
-import ImageCarousel from "./Components/ImageCarousel/ImageCarousel"
-
-
+import EncabezadoServicios from "./Components/EncabezadoServicios/EncabezadoServicios"
 export default function Home() {
-
   return (
     <>
-    <Navbar />
-    
+      <Navbar />
       <main className={styles.container}>
-      <div className={styles.barraazul}></div>
-        
-        <header id="inicio" className={styles.hero}>
-          <h1 className={styles.titulo}>
-          Servicio de Interventoria, Supervisión de Obras y Diseño Estructural en Colombia..
-          </h1>
-          <ImageCarousel/>
-          <p className={styles.heroparrafo}>
-            En Estructuras Verticales e Ingenieros SAS, nos especializamos en ofrecer soluciones integrales para la interventoría y supervisión de obras,
-            así como en el diseño y fabricación de montajes estructurales. 
-          </p>
-          <p className={styles.heroparrafouno}>
-            Además, somos expertos en la elaboración de pliegos de condiciones,
-            enfocados en el desarrollo de proyectos que cumplen con los más altos estándares técnicos y normativos.
-            </p>
-          <p className={styles.parrafodos}>
-           Nuestra experiencia abarca también el diseño estructural especializado y
-            el recibo de zonas comunes sometidas a propiedad horizontal (PH), garantizando que cada proyecto se ajuste a las regulaciones vigentes en Colombia.
-            Trabajamos bajo un riguroso cumplimiento de las normativas locales, lo que nos permite asegurar la calidad, seguridad y durabilidad de cada obra.
-          </p>
-          <p className={styles.parrafodos}>
-            Nos enorgullece ofrecer informes técnicos detallados que facilitan la toma de decisiones informadas, brindando a nuestros clientes la confianza de que sus proyectos cumplen
-            no solo con los más altos estándares de calidad, sino también con las regulaciones oficiales que rigen la industria de la construcción en Colombia.
-          </p>
-          <a href="tel:+573229578866" className={styles.cta}>Llámanos ahora</a>
-        </header>
-
+        <div className={styles.barraazul}></div>
+        <EncabezadoServicios />
         <section id="servicios" className={styles.services}>
           <h2>Nuestros Servicios</h2>
         </section>
@@ -55,16 +26,12 @@ export default function Home() {
         <section id="faq" className={styles.faq}>
           <FaqSection />
         </section>
-        
-
         <div className={styles.ctaContainer}>
           <h2 className={styles.ctaHeading}>¡Transforma tu proyecto con nosotros hoy mismo!</h2>
-          <p className={styles.ctaSubheading}>Contáctanos ahora y empieza a llevar tu negocio al siguiente nivel. Aportamos soluciones personalizadas y efectivas. 
+          <p className={styles.ctaSubheading}>Contáctanos ahora y empieza a llevar tu negocio al siguiente nivel. Aportamos soluciones personalizadas y efectivas.
             <br /> <br />¡No pierdas esta oportunidad única!</p>
-          <a href="tel:+573229578866" className={styles.ctaButton}>¡Quiero empezar ahora!</a>
+          <a href="tel:3229578866" className={styles.ctaButton}>¡Quiero empezar ahora!</a>
         </div>
-
-
         <section id="contacto" className={styles.contacto}>
           <ContactButtons />
           < Footer />
