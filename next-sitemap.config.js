d0524c18928aad92858951d5bec5d5d7948c933d
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-01-22 22:05:46 -0500

    actualizar todo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,5 @@ module.exports = {
     siteUrl: "https://estructuras-verticales-e-ingenieros-sas-five.vercel.app",
     generateRobotsTxt: true, // Esto genera el robots.txt automáticamente
     sitemapSize: 7000,  // Puedes ajustar este valor según la cantidad de URLs
-  outDir: './public', // El directorio de salida es 'public'
+  outDir: './public', // El directorio de salida es 'public'.
   };
