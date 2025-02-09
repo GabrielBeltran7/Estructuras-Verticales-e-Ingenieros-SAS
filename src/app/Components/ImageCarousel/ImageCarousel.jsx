964c3c7a3e2eb1280cfaab25b821453ce06c9a9a
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-02-09 12:10:04 -0500

    actualizacion imagenes webp

--- src/app/Components/ImageCarousel/ImageCarousel.jsx
+++ src/app/Components/ImageCarousel/ImageCarousel.jsx
@@ -4,11 +4,20 @@ import Image from "next/image";
 
 const ImageCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+
   const images = [
     "https://res.cloudinary.com/dby8lelja/image/upload/f_auto,q_auto/v1737605127/Estructuras%20Verticales%20e%20Ingenieros%20SAS/Servicio_de_Interventoria_jpkw6n.webp",
     "https://res.cloudinary.com/dby8lelja/image/upload/f_auto,q_auto/v1739112965/interventoria_y_supervision_de_obras_pkb2ck.webp",
     "https://res.cloudinary.com/dby8lelja/image/upload/f_auto,q_auto/v1737490967/Estructuras%20Verticales%20e%20Ingenieros%20SAS/Supervisi%C3%B3n_de_Obras_s463d0.webp",
   ];
+
+  useEffect(() => {
+    const checkScreenSize = () => setIsMobile(window.innerWidth <= 768);
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
 
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -33,12 +42,12 @@ const ImageCarousel = () => {
           <Image
             src={images[currentIndex]}
             alt={`Imagen ${currentIndex + 1}`}
-            width={900} 
-            height={550}
-            priority={currentIndex === 0} // La primera imagen se carga antes
+            width={isMobile ? 300 : 900} 
+            height={isMobile ? 200 : 550}
+            priority={!isMobile && currentIndex === 0} // Evita `priority` en móviles
             placeholder="blur" 
-            blurDataURL={images[currentIndex]} // Optimización con Cloudinary
-            fetchPriority="high" // Le da más importancia en la carga
+            blurDataURL={images[currentIndex]} 
+            sizes="(max-width: 768px) 300px, 900px"
             className={styles.image}
           />
         </div>
@@ -49,5 +58,6 @@ const ImageCarousel = () => {
 };
 
 export default ImageCarousel;
+
 
 
