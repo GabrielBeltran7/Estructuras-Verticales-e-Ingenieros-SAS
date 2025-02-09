577b35ac2a32ba4959df9a20bbfcbb46c8080396
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-02-09 10:19:43 -0500

    actuaizacion de imagenes

--- src/app/Components/ImageCarousel/ImageCarousel.jsx
+++ src/app/Components/ImageCarousel/ImageCarousel.jsx
@@ -6,7 +6,7 @@ const ImageCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const images = [
     "https://res.cloudinary.com/dby8lelja/image/upload/v1737605127/Estructuras%20Verticales%20e%20Ingenieros%20SAS/Servicio_de_Interventoria_jpkw6n.webp",
-    "https://res.cloudinary.com/dby8lelja/image/upload/v1737490967/Estructuras%20Verticales%20e%20Ingenieros%20SAS/Supervisi%C3%B3n_de_Obras_s463d0.webp",
+  
   ];
 
   const nextImage = () => {
