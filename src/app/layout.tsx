f8c281b8a9d8fb9b10e0dc90a562f3a78ce70e5f
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-02-16 00:13:23 -0500

    actualizar el celular del schema.org

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,7 +89,9 @@ export default function RootLayout({
                     "addressCountry": "CO",
                     "postalCode": "110111"  
                   },
-                  "telephone": "+57 3132581599"
+                  "telephone": "+57 3132581599",
+                  "image": "https://res.cloudinary.com/dby8lelja/image/upload/v1739641319/Estructuras%20Verticales%20e%20Ingenieros%20SAS/Imagenes%20Blog/Estructuras_Verticales_e_Ingenieros_SAS_qmjon6.webp"
+
                 },
                   {
                   "@type": "Service",
