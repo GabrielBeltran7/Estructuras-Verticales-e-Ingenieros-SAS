54190ab99ba865108323af88fda8fb267231290a
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-03-14 12:00:30 -0500

    actualizacion de video primcipal

--- src/app/Components/VideoComponents/VideoComponent.jsx
+++ src/app/Components/VideoComponents/VideoComponent.jsx
@@ -10,7 +10,7 @@ const VideoComponent = () => {
       {!isVideoLoaded ? (
         <div className={styles.videoPlaceholder} onClick={() => setIsVideoLoaded(true)}>
           <Image
-            src="https://res.cloudinary.com/dby8lelja/image/upload/v1741788875/Estructuras%20Verticales%20e%20Ingenieros%20SAS/Imagenes%20Blog/estructurasverticaleseingenierossas_itww2f.webp"
+            src="https://res.cloudinary.com/dby8lelja/image/upload/v1741971557/Estructuras%20Verticales%20e%20Ingenieros%20SAS/Estructuras_Verticales_e_Ingenieros_sas_idjptp.webp"
             alt="Ver video"
             width={800}  // ✅ Ajustar al mismo tamaño que el iframe
             height={450} // ✅ Mantener proporción 16:9
@@ -24,7 +24,7 @@ const VideoComponent = () => {
           className={styles.video}
           width="800"
           height="450"
-          src="https://www.youtube-nocookie.com/embed/6YjE9wgWJlw?autoplay=1&rel=0"
+          src="https://www.youtube-nocookie.com/embed/Kpj0nKqcb2c?autoplay=1&rel=0"
           title="Video Institucional | Estructuras Verticales e Ingenieros SAS"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
