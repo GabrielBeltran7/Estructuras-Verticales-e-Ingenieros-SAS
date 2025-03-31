aeac791fc4ed49d1355ef3507e843e146f27d3f8
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2025-03-31 07:52:19 -0500

    actualizacion de datos

--- next.config.ts
+++ next.config.ts
@@ -33,23 +33,3 @@ const nextConfig = withMDX({
 });
 
 module.exports = nextConfig;
-
-// const withMDX = require("@next/mdx")();
-
-// const nextConfig = withMDX({
-//   experimental: { mdxRs: true }, // Activa MDX en Next.js 15
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "img.youtube.com",
-//       },
-//     ],
-//   },
-// });
-
-// module.exports = nextConfig;
